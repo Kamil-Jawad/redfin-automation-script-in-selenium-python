531cfa6d1b8c3b715d3431c00935ed7a36da43dd
Kamiljawad <kamiljawad9@gmail.com>
2021-11-28 11:40:37 +0500

    first commit to add all the files

---
+++ redfin_main.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import time
+import re
+from re import search
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.maximize_window()  # For maximizing window
+driver.implicitly_wait(20)
+
+
+driver.get("https://www.redfin.com/")
+
+
+login_button = driver.find_element_by_css_selector(
+    'button[data-rf-test-name="SignInLink"]'
+)
+login_button.click()
+
+enter_email = driver.find_element_by_css_selector('input[name="emailInput"]')
+time.sleep(10)
+email = input("enter email :")
+# zmoha@wurric.site
+enter_email.send_keys(email)
+submit_email = driver.find_element_by_css_selector(
+    'button[class="button Button submitButton primary"]')
+submit_email.click()
+enter_password = driver.find_element_by_css_selector('input[class="password"]')
+time.sleep(10)
+password = input("enter password :")
+# redfin@123
+enter_password.send_keys(password)
+logged_in = driver.find_element_by_css_selector(
+    'button[data-rf-test-name="submitButton"]')
+logged_in.click()
+driver.implicitly_wait(30)
+# enter_search = driver.find_element_by_css_selector(
+#     'input[class="search-input-box"]')
+
+driver.refresh()
+
+enter_search = driver.find_element_by_css_selector(
+    'input[class="search-input-box"]')
+time.sleep(15)
+search = input("enter search : ")
+enter_search.send_keys(search)
+
+
+click_search = driver.find_element_by_css_selector(
+    'button[data-rf-test-name="searchButton"]')
+click_search.click()
+
+# heading = driver.find_element_by_css_selector(
+#     'h1[data-rf-test-id="h1-header"]')
+# print(sixth.text)
+# el1 = driver.find_element_by_css_selector('div[class="bottomV2 "]')
+# el2 = driver.find_element_by_css_selector('span[class="homecardV2Price"]')
+# print(el2.text)
+
+# complete = driver.find_elements_by_css_selector(
+#     'div[class="PhotosView bg-color-white above-the-fold"]')
+# print(complete.text)
+
+# scrape items
+price = []
+stats = []
+address = []
+page_count = driver.find_elements_by_css_selector(
+    'a[class="clickable goToPage"]')
+for it in range(len(page_count)):
+
+    for p in driver.find_elements_by_css_selector(
+            'span[data-rf-test-name="homecard-price"]'):
+        card_price = p.text
+        price.append(card_price)
+
+    st = driver.find_elements_by_css_selector('div[class="bottomV2 "]')
+    for i in st:
+        card_stats = i.find_element_by_css_selector(
+            'div[class="HomeStatsV2 font-size-small "]').text
+        n_text = re.sub('[\n]', ' ', card_stats)
+        final_text = re.sub(',', ' ', n_text)
+        stats.append(final_text)
+    for ad in st:
+
+        card_add = ad.find_element_by_css_selector(
+            'span[data-rf-test-id="abp-streetLine"]').text
+        address.append(card_add)
+    print('page {0} is extracted...'.format(it))
+    next = driver.find_element_by_css_selector(
+        'button[data-rf-test-id="react-data-paginate-next"]')
+    next.click()
+    time.sleep(10)
+
+
+all_houses = [price, [i for i in stats], address]
+
+# convert into dataframe
+house_details = pd.DataFrame(all_houses)
+house_details = house_details.T
+house_details = house_details.rename(
+    columns={0: 'price', 1: 'house_statistics', 2: 'address'})
+# print(house_details.columns)
+print(house_details.shape)
+print()
+print(house_details['house_statistics'][1:2])
+print()
+
+# saving into csv
+try:
+    house_details.to_csv('D:\\DataSets\\' + search + '.csv', index=False)
+    print("file is saved successfully.")
+except:
+    print("there is an error to save the file")
+
+
+# with open("test.csv", 'w') as file:
+#     writer = csv.writer(file)
+#     writer.writerows(all_houses)
